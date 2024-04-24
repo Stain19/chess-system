a4c11a25d89735791e3d7a93fc51ad13716c8e8b
Pedro Braga <prometeuslbo@gmail.com>
2024-04-24 12:19:42 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,11 +7,24 @@ import gameboard.Position;
 import chess.pieces.*;
 
 public class ChessMatch {
+    private Integer turn;
+    private Color currentPlayer;
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
+
         initialSetup();
+    }
+
+    public Integer getTurn(){
+        return turn;
+    }
+
+    public Color getCurrentPlayer(){
+        return currentPlayer;
     }
 
     public ChessPiece[][] getPieces() {
@@ -33,6 +46,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
@@ -46,14 +60,24 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position))
             throw new ChessException("There is no piece on source position");
-        if (!board.piece(position).isThereAnyPossibleMove()) {
+        if (!isCurrentPlayerColor(position))
+            throw new ChessException("The chosen piece is not yours");
+        if (!board.piece(position).isThereAnyPossibleMove())
             throw new ChessException("There is no possible movements for the chosen pieces");
-        }
     }
 
     private void validateTargetPosition(Position source, Position target) {
         if (!board.piece(source).possibleMove(target))
             throw new ChessException("The chosen piece can not move to target position");
+    }
+
+    private void nextTurn(){
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE)? Color.BLACK : Color.WHITE;
+    }
+
+    private boolean isCurrentPlayerColor(Position position){
+        return currentPlayer == ((ChessPiece)board.piece(position)).getColor();
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
