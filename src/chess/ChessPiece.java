318a61e98606acc577dabcfc8508d9feef474eba
Pedro Braga <prometeuslbo@gmail.com>
2024-04-26 23:35:39 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,8 +4,11 @@ import gameboard.Board;
 import gameboard.Piece;
 import gameboard.Position;
 
+import java.util.Set;
+
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -15,6 +18,19 @@ public abstract class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
+
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+
+    public void decreaseMoveCount(){
+        moveCount--;
+    }
+
 
     public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
